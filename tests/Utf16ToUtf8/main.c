aff13db126b72a4a45fc615b36c899e82c1c671c
Christoph Charles <c.charles.dev@gmail.com>
2018-10-14 09:35:47 +0200

    Added a few tests for Utf8<->Utf16 conversion

--- tests/Utf16ToUtf8/main.c
+++ tests/Utf16ToUtf8/main.c
@@ -21,8 +21,109 @@
 // SOFTWARE.
 
 #include "../../include/ccunicode.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int TestEmptyString(void)
+{
+    const char EmptyStr[] = "";
+    const uint16_t EmptyWStr[] = {0};
+
+    uint8_t *Str;
+    int Count = ccunicode_Utf16ToUtf8(EmptyWStr, &Str);
+    if (Count < 0)
+    {
+        fprintf(stderr, "Error %d in ccunicode_Utf16ToUtf8", Count);
+        return -1;
+    }
+    if (Count+1 != sizeof(EmptyStr)/sizeof(*EmptyStr))
+    {
+        fprintf(stderr, "Mismatch between expected output size (%d) and effective output size (%d).", (int)(sizeof(EmptyStr)/sizeof(*EmptyStr)), Count+1);
+        free(Str);
+        return -1;
+    }
+    if (memcmp(EmptyStr, Str, (Count+1)*sizeof(*Str)))
+    {
+        fprintf(stderr, "Empty string code point does not give single 0 codepoint");
+        free(Str);
+        return -1;
+    }
+
+    free(Str);
+    return 0;
+}
+
+int TestHelloWorldString(void)
+{
+    const char HelloWorldStr[] = "Hello World !";
+    const uint16_t HelloWorldWStr[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', ' ', '!', 0};
+
+    uint8_t *Str;
+    int Count = ccunicode_Utf16ToUtf8(HelloWorldWStr, &Str);
+    if (Count < 0)
+    {
+        fprintf(stderr, "Error %d in ccunicode_Utf16ToUtf8", Count);
+        return -1;
+    }
+    if (Count+1 != sizeof(HelloWorldStr)/sizeof(*HelloWorldStr))
+    {
+        fprintf(stderr, "Mismatch between expected output size (%d) and effective output size (%d).", (int)(sizeof(HelloWorldStr)/sizeof(*HelloWorldStr)), Count+1);
+        free(Str);
+        return -1;
+    }
+    if (memcmp(HelloWorldStr, Str, (Count+1)*sizeof(*Str)))
+    {
+        fprintf(stderr, "Mismatch for wide chars for Hello World string");
+        free(Str);
+        return -1;
+    }
+
+    free(Str);
+    return 0;
+}
+
+int TestTrueUtf16String(void)
+{
+    const char TrueUtf16Str[] = "\u00C9\u0800\U00010000";
+    const uint16_t TrueUtf16WStr[] = {0xC9, 0x800, 0xD800, 0xDC00, 0};
+
+    uint8_t *Str;
+    int Count = ccunicode_Utf16ToUtf8(TrueUtf16WStr, &Str);
+    if (Count < 0)
+    {
+        fprintf(stderr, "Error %d in ccunicode_Utf16ToUtf8", Count);
+        return -1;
+    }
+    if (Count+1 != sizeof(TrueUtf16Str)/sizeof(*TrueUtf16Str))
+    {
+        fprintf(stderr, "Mismatch between expected output size (%d) and effective output size (%d).", (int)(sizeof(TrueUtf16Str)/sizeof(*TrueUtf16Str)), Count+1);
+        free(Str);
+        return -1;
+    }
+    if (memcmp(TrueUtf16Str, Str, (Count+1)*sizeof(*Str)))
+    {
+        fprintf(stderr, "Mismatch for codepoints for True Utf8 string");
+        free(Str);
+        return -1;
+    }
+
+    free(Str);
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
+    int Res = 0;
+
+#define TEST(t) \
+    Res = t(); \
+    if (Res) \
+        return Res;
+
+    TEST(TestEmptyString)
+    TEST(TestHelloWorldString)
+    TEST(TestTrueUtf16String)
+
     return 0;
 }
--- tests/Utf8ToUtf16/main.c
+++ tests/Utf8ToUtf16/main.c
@@ -21,8 +21,109 @@
 // SOFTWARE.
 
 #include "../../include/ccunicode.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int TestEmptyString(void)
+{
+    const char EmptyStr[] = "";
+    const uint16_t EmptyWStr[] = {0};
+
+    uint16_t *WStr;
+    int Count = ccunicode_Utf8ToUtf16(EmptyStr, &WStr);
+    if (Count < 0)
+    {
+        fprintf(stderr, "Error %d in ccunicode_Utf8ToUtf16", Count);
+        return -1;
+    }
+    if (Count+1 != sizeof(EmptyWStr)/sizeof(*EmptyWStr))
+    {
+        fprintf(stderr, "Mismatch between expected output size (%d) and effective output size (%d).", (int)(sizeof(EmptyWStr)/sizeof(*EmptyWStr)), Count+1);
+        free(WStr);
+        return -1;
+    }
+    if (memcmp(EmptyWStr, WStr, (Count+1)*sizeof(*WStr)))
+    {
+        fprintf(stderr, "Empty string code point does not give single 0 codepoint");
+        free(WStr);
+        return -1;
+    }
+
+    free(WStr);
+    return 0;
+}
+
+int TestHelloWorldString(void)
+{
+    const char HelloWorldStr[] = "Hello World !";
+    const uint16_t HelloWorldWStr[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', ' ', '!', 0};
+
+    uint16_t *WStr;
+    int Count = ccunicode_Utf8ToUtf16(HelloWorldStr, &WStr);
+    if (Count < 0)
+    {
+        fprintf(stderr, "Error %d in ccunicode_Utf8ToUtf16", Count);
+        return -1;
+    }
+    if (Count+1 != sizeof(HelloWorldWStr)/sizeof(*HelloWorldWStr))
+    {
+        fprintf(stderr, "Mismatch between expected output size (%d) and effective output size (%d).", (int)(sizeof(HelloWorldWStr)/sizeof(*HelloWorldWStr)), Count+1);
+        free(WStr);
+        return -1;
+    }
+    if (memcmp(HelloWorldWStr, WStr, (Count+1)*sizeof(*WStr)))
+    {
+        fprintf(stderr, "Mismatch for wide chars for Hello World string");
+        free(WStr);
+        return -1;
+    }
+
+    free(WStr);
+    return 0;
+}
+
+int TestTrueUtf8String(void)
+{
+    const char TrueUtf8Str[] = "\u00C9\u0800\U00010000";
+    const uint16_t TrueUtf8WStr[] = {0xC9, 0x800, 0xD800, 0xDC00, 0};
+
+    uint16_t *WStr;
+    int Count = ccunicode_Utf8ToUtf16(TrueUtf8Str, &WStr);
+    if (Count < 0)
+    {
+        fprintf(stderr, "Error %d in ccunicode_Utf8ToUtf16", Count);
+        return -1;
+    }
+    if (Count+1 != sizeof(TrueUtf8WStr)/sizeof(*TrueUtf8WStr))
+    {
+        fprintf(stderr, "Mismatch between expected output size (%d) and effective output size (%d).", (int)(sizeof(TrueUtf8WStr)/sizeof(*TrueUtf8WStr)), Count+1);
+        free(WStr);
+        return -1;
+    }
+    if (memcmp(TrueUtf8WStr, WStr, (Count+1)*sizeof(*WStr)))
+    {
+        fprintf(stderr, "Mismatch for codepoints for True Utf8 string");
+        free(WStr);
+        return -1;
+    }
+
+    free(WStr);
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
+    int Res = 0;
+
+#define TEST(t) \
+    Res = t(); \
+    if (Res) \
+        return Res;
+
+    TEST(TestEmptyString)
+    TEST(TestHelloWorldString)
+    TEST(TestTrueUtf8String)
+
     return 0;
 }
